ab2bfb3447157d32f662b5f67dcf09c4e6821fe4
liyi <1150489263@qq.com>
2025-03-14 00:29:48 +0800

    fix(wifi):完善wifi密码输入连接功能

--- main/APP/WIFI/wifi_app.c
+++ main/APP/WIFI/wifi_app.c
@@ -1,11 +1,70 @@
 #include <stdio.h>
 #include "wifi_app.h"
+#include "esp_wifi.h"
+#include "freertos/event_groups.h"
+#include "esp_event.h"
 
 #define DEFAULT_SCAN_LIST_SIZE 10
 static const char *TAG = "WIFI_APP";
+
+lv_obj_t *wifi_password_page; 
+lv_obj_t *label_wifi_name; 
+lv_obj_t *ta_pass_text;
+lv_obj_t *label_wifi_connect;
+
 lv_obj_t* wifi_scan_page;
 lv_obj_t* wifi_list;
-QueueHandle_t xQueueWifiAccount = NULL;
+
+// wifi事件组
+static EventGroupHandle_t s_wifi_event_group;
+// wifi事件
+#define WIFI_CONNECTED_BIT    BIT0
+#define WIFI_FAIL_BIT         BIT1
+#define WIFI_START_BIT        BIT2
+// wifi最大重连次数
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+
+// wifi账号队列
+static QueueHandle_t xQueueWifiAccount = NULL;
+// 队列要传输的内容
+typedef struct {
+    char wifi_ssid[32];  // 获取wifi名称
+    char wifi_password[64]; // 获取wifi密码         
+} wifi_account_t;
+
+static void btn_connect_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        ESP_LOGI(TAG, "OK Clicked");
+        const char *wifi_ssid = lv_label_get_text(label_wifi_name);
+        const char *wifi_password = lv_textarea_get_text(ta_pass_text);
+        if(*wifi_password != '\0') // 判断是否为空字符串
+        {
+            wifi_account_t wifi_account;
+            strcpy(wifi_account.wifi_ssid, wifi_ssid);
+            strcpy(wifi_account.wifi_password, wifi_password);
+            ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                    wifi_account.wifi_ssid, wifi_account.wifi_password);
+            //lv_wifi_connect(); // 显示wifi连接界面
+
+            lv_obj_del(wifi_password_page);
+            // 发送WiFi账号密码信息到队列
+            xQueueSend(xQueueWifiAccount, &wifi_account, portMAX_DELAY); 
+        }
+    }
+}
+
+static void btn_back_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        ESP_LOGI(TAG, "btn_back Clicked");
+        lv_obj_del(wifi_password_page); // 删除密码输入界面
+    }
+}
 
 static void list_btn_cb(lv_event_t * e)
 {
@@ -19,7 +78,7 @@ static void list_btn_cb(lv_event_t * e)
     }
 
     // 创建密码输入页面
-    lv_obj_t *wifi_password_page = lv_obj_create(lv_scr_act());
+    wifi_password_page = lv_obj_create(lv_scr_act());
     lv_obj_set_size(wifi_password_page, 320, 240);
     lv_obj_set_style_border_width(wifi_password_page, 0, 0); // 设置边框宽度
     lv_obj_set_style_pad_all(wifi_password_page, 0, 0);  // 设置间隙
@@ -33,7 +92,7 @@ static void list_btn_cb(lv_event_t * e)
     lv_obj_set_style_pad_all(btn_back, 0, 0);  // 设置间隙
     lv_obj_set_style_bg_opa(btn_back, LV_OPA_TRANSP, LV_PART_MAIN); // 背景透明
     lv_obj_set_style_shadow_opa(btn_back, LV_OPA_TRANSP, LV_PART_MAIN); // 阴影透明
-    //lv_obj_add_event_cb(btn_back, btn_back_cb, LV_EVENT_ALL, NULL); // 添加按键处理函数
+    lv_obj_add_event_cb(btn_back, btn_back_cb, LV_EVENT_ALL, NULL); // 添加按键处理函数
 
     lv_obj_t *label_back = lv_label_create(btn_back);
     lv_label_set_text(label_back, LV_SYMBOL_LEFT);  // 按键上显示左箭头符号
@@ -41,13 +100,13 @@ static void list_btn_cb(lv_event_t * e)
     lv_obj_align(label_back, LV_ALIGN_TOP_LEFT, 10, 10);
 
     // 显示选中的wifi名称
-    lv_obj_t *label_wifi_name = lv_label_create(wifi_password_page);
+    label_wifi_name = lv_label_create(wifi_password_page);
     lv_obj_set_style_text_font(label_wifi_name, &lv_font_montserrat_14, 0);
     lv_label_set_text(label_wifi_name, wifi_name);
     lv_obj_align(label_wifi_name, LV_ALIGN_TOP_MID, 0, 10);
 
     // 创建密码输入框
-    lv_obj_t *ta_pass_text = lv_textarea_create(wifi_password_page);
+    ta_pass_text = lv_textarea_create(wifi_password_page);
     lv_obj_set_style_text_font(ta_pass_text, &lv_font_montserrat_14, 0);
     lv_textarea_set_one_line(ta_pass_text, true);  // 一行显示
     lv_textarea_set_password_mode(ta_pass_text, false); // 是否使用密码输入显示模式
@@ -60,7 +119,7 @@ static void list_btn_cb(lv_event_t * e)
     lv_obj_t *btn_connect = lv_btn_create(wifi_password_page);
     lv_obj_align(btn_connect, LV_ALIGN_TOP_LEFT, 170, 40);
     lv_obj_set_width(btn_connect, 65); // 宽度
-    //lv_obj_add_event_cb(btn_connect, btn_connect_cb, LV_EVENT_ALL, NULL); // 事件处理函数
+    lv_obj_add_event_cb(btn_connect, btn_connect_cb, LV_EVENT_ALL, NULL); // 事件处理函数
 
     lv_obj_t *label_ok = lv_label_create(btn_connect);
     lv_label_set_text(label_ok, "OK");
@@ -88,9 +147,35 @@ static void list_btn_cb(lv_event_t * e)
 
 }
 
-
-static void wifi_scan(wifi_ap_record_t *info,uint16_t *number)
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
 {
+    static int s_retry_num = 0;
+
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        xEventGroupSetBits(s_wifi_event_group, WIFI_START_BIT);
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+
+static void wifi_scan(wifi_ap_record_t ap_info[], uint16_t *ap_number)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
@@ -98,24 +183,78 @@ static void wifi_scan(wifi_ap_record_t *info,uint16_t *number)
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+    
     uint16_t ap_count = 0;
+    
+    memset(ap_info, 0, *ap_number * sizeof(wifi_ap_record_t));
 
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_start());
     esp_wifi_scan_start(NULL, true);
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(number, info));
 
-    // for (uint16_t i = 0; i < DEFAULT_SCAN_LIST_SIZE; i++) {
-    //     ESP_LOGI(TAG, "SSID \t\t%s", info[i].ssid);
-    //     ESP_LOGI(TAG, "RSSI \t\t%d", info[i].rssi);
-    //     print_auth_mode(info[i].authmode);
-    //     if (info[i].authmode != WIFI_AUTH_WEP) {
-    //         print_cipher_type(info[i].pairwise_cipher, info[i].group_cipher);
-    //     }
-    //     ESP_LOGI(TAG, "Channel \t\t%d\n", info[i].primary);
-    // }
+    ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", *ap_number);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));  // 获取扫描到的wifi数量
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(ap_number, ap_info)); // 获取真实的获取到wifi数量和信息
+    ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, *ap_number);
 
+}
+
+static void wifi_connect(void *arg)
+{
+    wifi_account_t wifi_account;
+
+    while (true)
+    {
+        // 如果收到wifi账号队列消息
+        if(xQueueReceive(xQueueWifiAccount, &wifi_account, portMAX_DELAY))
+        {
+            wifi_config_t wifi_config = {
+                .sta = {
+                    .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+                    .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+                    .sae_h2e_identifier = "",
+                    },
+            };
+            strcpy((char *)wifi_config.sta.ssid, wifi_account.wifi_ssid);
+            strcpy((char *)wifi_config.sta.password, wifi_account.wifi_password);
+            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+            ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 wifi_config.sta.ssid, wifi_config.sta.password);
+            esp_wifi_connect();
+            /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+            * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+            EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+            /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+            * happened. */
+            if (bits & WIFI_CONNECTED_BIT) {
+                ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                        wifi_config.sta.ssid, wifi_config.sta.password);
+            } else if (bits & WIFI_FAIL_BIT) {
+                ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                        wifi_config.sta.ssid, wifi_config.sta.password);
+            } else {
+                ESP_LOGE(TAG, "UNEXPECTED EVENT");
+            }
+        }
+    }
 }
 
 void app_wifi_connect(void)
@@ -165,6 +304,6 @@ void app_wifi_connect(void)
     //lvgl_port_unlock();
 
     // 创建wifi连接任务
-    //xQueueWifiAccount = xQueueCreate(2, sizeof(wifi_account_t));
-    //xTaskCreatePinnedToCore(wifi_connect, "wifi_connect", 4 * 1024, NULL, 5, NULL, 1);  // 创建wifi连接任务
+    xQueueWifiAccount = xQueueCreate(2, sizeof(wifi_account_t));
+    xTaskCreatePinnedToCore(wifi_connect, "wifi_connect", 4 * 1024, NULL, 5, NULL, 1);  // 创建wifi连接任务
 }
